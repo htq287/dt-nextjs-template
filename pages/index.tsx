4c175af89abc87e4201923f78d93059086ca1591
Hung Q. Truong <htq287@gmail.com>
2020-09-25 10:38:28 +0700

    * initialize project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>Welcome Hung Q. Truong 's Homepage</div>
+        );
+    }
+}
+
+export default Index;
